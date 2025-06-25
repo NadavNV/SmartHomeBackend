d88eb155481a6c78be7a5ccfb1e00ead63643bae
yarden-ziv <yardenziv1@gmail.com>
2025-06-25 13:32:02 +0300

    stop tracking .env file

--- Test/test.py
+++ Test/test.py
@@ -1,1 +1,27 @@
-print ("test script")
+import requests
+import sys
+
+data = {
+      "id": "test-device",
+      "type": "test_device",
+      "name": "Test Device",
+      "room": "Test",
+      "status": "off",
+      "parameters": {
+        "test": 40
+      }
+    }
+
+# Get current devices list
+response = requests.get("http://localhost:5200/api/devices")
+if 199 > response.status_code > 400:
+    print("API is responding")
+else:
+    print("API is not up")
+    sys.exit(1)
+
+# Add a new test device
+requests.post("http://localhost:5200/api/devices", json = data)
+
+# Check if the new device was added
+response = requests.get("http://localhost:5200/api/devices")
